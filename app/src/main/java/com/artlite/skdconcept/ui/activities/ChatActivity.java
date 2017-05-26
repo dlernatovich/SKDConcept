402af95db3dbc6136417a721a2c80f975295d2c4
vostroglyad <vostroglyad@geeksforless.net>
2017-05-26 16:04:32 +0300

    added chat activity

---
+++ app/src/main/java/com/artlite/skdconcept/ui/activities/ChatActivity.java
@@ -0,0 +1,55 @@
+package com.artlite.skdconcept.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
+import com.artlite.bslibrary.ui.activity.BSActivity;
+import com.artlite.skdconcept.R;
+
+/**
+ * Created by Slava on 5/26/17.
+ */
+
+public class ChatActivity extends BSActivity {
+
+    /**
+     * Method which provide the getting of the layout ID for the current Activity
+     *
+     * @return layout ID for the current Activity
+     */
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_chat;
+    }
+
+    /**
+     * Method which provide the action when Activity is created
+     *
+     * @param bundle instance of {@link Bundle}
+     */
+    @Override
+    protected void onCreateActivity(@Nullable Bundle bundle) {
+        setTitle("Chat");
+    }
+
+    /**
+     * Method which provide the defining if need to override of the transition animation
+     *
+     * @return defining results
+     */
+    @Override
+    protected boolean isOverrideTransitionAnimation() {
+        return true;
+    }
+
+    /**
+     * Method which provide the checking if need back button into {@link ActionBar}
+     *
+     * @return checking if need back button into {@link ActionBar}
+     */
+    @Override
+    protected boolean isNeedBackButton() {
+        return true;
+    }
+}
